083b595f5e3ca70c7b2752521feecf68e41c2329
Danny49210 <danielstefan107@gmail.com>
2024-05-21 19:38:54 +0300

    Design Pattern Factory

---
+++ src/main/java/org/example/factory/Computer.java
@@ -0,0 +1,8 @@
+package org.example.factory;
+
+public abstract class Computer {
+
+    public abstract String getRam();
+    public abstract String getHardDisk();
+
+}
---
+++ src/main/java/org/example/factory/Server.java
@@ -0,0 +1,23 @@
+package org.example.factory;
+
+public class Server extends Computer{
+
+    private Integer ramPerUnit;
+    private Integer hardDiskPerUnit;
+    private Integer units;
+    @Override
+    public String getRam() {
+        return "ram server: " + ramPerUnit * units;
+    }
+
+    @Override
+    public String getHardDisk() {
+        return "hardDisk server: " + hardDiskPerUnit * units;
+    }
+
+    public Server(Integer ramPerUnit, Integer hardDiskPerUnit, Integer units) {
+        this.ramPerUnit = ramPerUnit;
+        this.hardDiskPerUnit = hardDiskPerUnit;
+        this.units = units;
+    }
+}
