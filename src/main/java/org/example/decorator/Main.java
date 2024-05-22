7765a65c09481c0031347cea32b3afb3100f6ef4
Danny49210 <danielstefan107@gmail.com>
2024-05-22 20:47:29 +0300

    Design Pattern Decorator

---
+++ src/main/java/org/example/decorator/Main.java
@@ -0,0 +1,15 @@
+package org.example.decorator;
+
+public class Main {
+    public static void main(String[] args) {
+        Car dacia = new BasicCar("red", 288);
+        dacia.assemble();
+        Car luxuryCar = new LuxuryCar(dacia,true);
+        luxuryCar.assemble();
+        Car sportLuxuryCar = new SportsCar(luxuryCar, 150);
+        sportLuxuryCar.assemble();
+        Car sandero = new BasicCar("green", 90);
+        Car sportCar = new SportsCar(sandero, 90);
+        sportCar.assemble();
+    }
+}
