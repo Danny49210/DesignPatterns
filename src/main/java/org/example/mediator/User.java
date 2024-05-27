7735a729cf8bd7e2c4f7bdcc1358bc8efe255b61
Danny49210 <danielstefan107@gmail.com>
2024-05-27 21:02:04 +0300

    Design Pattern Observer - List<> si HashMap<>

---
+++ src/main/java/org/example/mediator/User.java
@@ -0,0 +1,14 @@
+package org.example.mediator;
+
+public  abstract class User {
+    protected String name;
+    protected ChatMediator chat;
+
+    public User(String name, ChatMediator chat) {
+        this.name = name;
+        this.chat = chat;
+    }
+
+    public abstract void send(String message);
+    public abstract void receive(String message);
+}
